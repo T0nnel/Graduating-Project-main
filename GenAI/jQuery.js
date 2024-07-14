f5dad7290c6048e226041f0048c755f89587a1ef
Tonnel <154825284+T0nnel@users.noreply.github.com>
2024-07-14 15:30:37 +0300

    Initial commit

---
+++ GenAI/jQuery.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $("#messageArea").on("submit", function(event) {
+        // Handling user input and sending it to the server
+        // Handling server response and updating the chat interface
+    });
+});
